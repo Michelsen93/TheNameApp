4b813daa74db3e585821f7288bc49eb195c9e804
ole-martin michelsen <ole-martin michelsen>
2017-01-12 10:26:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/thenameapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thenameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int i = 0;
+        
+        setContentView(R.layout.activity_main);
+    }
+}
