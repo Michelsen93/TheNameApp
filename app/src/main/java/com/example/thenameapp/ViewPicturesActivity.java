d37e787825ff363251922bb20124d7d6f3ba73d4
Michelsen93 <om.michelsen@gmail.com>
2017-01-12 13:36:21 +0100

    added picture to drawable.

--- app/src/main/java/com/example/thenameapp/ViewPicturesActivity.java
+++ app/src/main/java/com/example/thenameapp/ViewPicturesActivity.java
@@ -1,5 +1,8 @@
 package com.example.thenameapp;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,8 +12,8 @@ public class ViewPicturesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i = 0;
         setContentView(R.layout.activity_view_pictures);
-        ImageView i = new ImageView(this);
-        i.setImageResource(R.drawable.sample);
+
     }
 }
