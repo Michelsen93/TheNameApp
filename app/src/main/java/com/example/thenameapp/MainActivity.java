7fcecd78e1557fd11773bb4e96ff83610adb733b
Michelsen93 <om.michelsen@gmail.com>
2017-01-12 10:55:38 +0100

    Added buttons to main activity

--- app/src/main/java/com/example/thenameapp/MainActivity.java
+++ app/src/main/java/com/example/thenameapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.thenameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
         int i = 0;
 
         setContentView(R.layout.activity_main);
+    }
+    public void addPerson(View view){
+        //TODO
+        //Go to add person activity
+    }
+    public void viewNames(View view){
+        //TODO
+        //Go to viewNames activity
+    }
+    public void viewPictures(View view){
+        //TODO
+        //Go to viewPictures activity
+    }
+    public void learningMode(View view){
+        //TODO
+        //Go to learningMode activity
     }
 }
