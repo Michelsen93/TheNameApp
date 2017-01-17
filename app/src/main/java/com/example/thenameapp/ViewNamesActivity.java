6e2dd9979c5a3fc25663acb5089241c88a0b3c5c
Michelsen93 <om.michelsen@gmail.com>
2017-01-17 16:00:42 +0100

    ViewNames main function working. Missing beauty touch

--- app/src/main/java/com/example/thenameapp/ViewNamesActivity.java
+++ app/src/main/java/com/example/thenameapp/ViewNamesActivity.java
@@ -1,9 +1,13 @@
 package com.example.thenameapp;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -14,28 +18,29 @@ public class ViewNamesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_view_names);
-        String[] persons = {"sample", "sample2", "sample3"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, persons);
+
+        String[] names = {"sample", "sample2", "sample3"};
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, names);
         ListView lw = (ListView) findViewById(R.id.listView);
+        lw.setAdapter(adapter);
+        lw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+
+                String uri = "@drawable/" + parent.getItemAtPosition(position);
+                Resources res = getResources();
+                int resID = res.getIdentifier(uri , "drawable", getPackageName());
+                ImageView iv = (ImageView)findViewById(R.id.imageLoad);
+                Bitmap pic = BitmapFactory.decodeResource(getResources(), resID);
+                int nh = (int) ( pic.getHeight() * (512.0 / pic.getWidth()) );
+
+                Bitmap scaled = Bitmap.createScaledBitmap(pic, 512, nh, true);
+                iv.setImageBitmap(scaled);
+
+            }
+        });
 
     }
-    public void showImage(View view){
-        //TODO LOAD PICTURE CORRESPONDING TO NAME
-        CharSequence name = view.getContentDescription();
-        String uri = "@drawable/" + name;
-        Resources res = getResources();
-        int resID = res.getIdentifier(uri , "drawable", getPackageName());
-        ImageView iv = new ImageView(this);
-        iv.setImageResource(resID);
 
-        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        p.addRule(RelativeLayout.BELOW, view.getId());
-        iv.setLayoutParams(p);
-        RelativeLayout layout =  (RelativeLayout) findViewById(R.id.activity_view_names);
-        layout.addView(iv);
-    }
 }
