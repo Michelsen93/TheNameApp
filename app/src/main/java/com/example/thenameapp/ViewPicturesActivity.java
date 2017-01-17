50d24ff4a24c340907cc18bd18f3ab2d8728ccee
Michelsen93 <om.michelsen@gmail.com>
2017-01-17 10:23:35 +0100

    Added text on click for imageView

--- app/src/main/java/com/example/thenameapp/ViewPicturesActivity.java
+++ app/src/main/java/com/example/thenameapp/ViewPicturesActivity.java
@@ -1,6 +1,7 @@
 package com.example.thenameapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,7 +9,11 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.io.InputStream;
 
@@ -17,9 +22,21 @@ public class ViewPicturesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Context context = getApplicationContext();
-        String uri = "@drawable/sample.png";
+        int i =0;
+
         setContentView(R.layout.activity_view_pictures);
 
+    }
+    public void showText(View view){
+        CharSequence name = view.getContentDescription();
+        TextView text = new TextView(this);
+        text.setText(name);
+        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        p.addRule(RelativeLayout.BELOW, view.getId());
+        text.setLayoutParams(p);
+        RelativeLayout layout =  (RelativeLayout) findViewById(R.id.activity_view_pictures);
+        layout.addView(text);
     }
 }
